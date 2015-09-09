cbb05ed9cefa81a69c84d84f70ad8d815ed2dba2
intanzahara <intanzrr@gmail.com>
2015-09-09 19:46:10 +0700

    Update banana.hs

--- banana.hs
+++ banana.hs
@@ -2,7 +2,13 @@ module Banana where
 
 import Data.List
 
--- 1.a
+-- 1.a/
+
+
+null' [] = True
+null' n = False
+
+--pembatas
 
 fst' (a,b) = a
 
@@ -12,8 +18,16 @@ snd' (a,b) = b
 
 --pembatas
 
+
+head' (x:xs) = x
+
+--pembatas
+
 length' [] = 0
 length' (x:xs) = 1 + (length' xs)
 
 --pembatas
 
+last' (x:xs) = xs
+
+--pembatas
