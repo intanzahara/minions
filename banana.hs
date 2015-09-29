62d64cc2d402ac8da4e7e7f51ac22b03968ebed7
intanzahara <intanzrr@gmail.com>
2015-09-29 23:04:32 +0700

    Update banana.hs

--- banana.hs
+++ banana.hs
@@ -10,6 +10,18 @@ null' n = False
 
 --pembatas
 
+take' _ [] = []
+take' 0 (x:xs) = []
+take' n (x:xs) = x : take' (n-1) xs
+
+--pembatas
+
+drop' _ [] = []
+drop' 0 (x:xs) = (x:xs)
+drop' n (x:xs) = drop' (n-1) xs
+
+--pembatas
+
 fst' (a,b) = a
 
 --pembatas
@@ -17,7 +29,6 @@ fst' (a,b) = a
 snd' (a,b) = b
 
 --pembatas
-
 
 head' (x:xs) = x
 
@@ -28,6 +39,23 @@ length' (x:xs) = 1 + (length' xs)
 
 --pembatas
 
-last' (x:xs) = xs
+last' [a] = a
+last' (x:xs) = last' xs
+
+--pembatas
+
+tail' (x:xs) = xs
+
+--pembatas
+
+max' x y
+  | x >= y = x
+  | y >= x = y
+
+--pembatas
+
+min' x y
+  | x <= y = x
+  | y <= x = y
 
 --pembatas
